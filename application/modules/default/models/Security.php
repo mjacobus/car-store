07eed026e1b46006bf2cb2b0636e973cf174d509
Marcelo G. Jacobus Jr <marcelo.jacobus@gmail.com>
2010-11-16 13:53:00 -0200

    Added Model_Security

---
+++ application/modules/default/models/Security.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Description of Password
+ *
+ * @author marcelo.jacobus
+ */
+class Model_Security
+{
+
+    /**
+     * Get string and return a hash
+     * @param string $string
+     * @return string
+     */
+    public static function stringToPasswordHash($string)
+    {
+        if (strlen($string) != 40) {
+            $string = sha1($string);
+        }
+        $salt = self::getPasswordSalt();
+        return sha1($salt . $string . $salt);
+    }
+
+    /**
+     * Get password salt
+     * @return string
+     */
+    public static function getPasswordSalt()
+    {
+        return Zend_Registry::get('securitySalt');
+    }
+
+    /**
+     * Generate a new Token
+     * @param string $tokenString string to modify token
+     * @return string
+     */
+    public static function getNewToken($tokenString = '')
+    {
+        $tokenString .= self::getPasswordSalt();
+        return sha1($tokenString . time() . $tokenString);
+    }
+
+    /**
+     * Get confirmation for a token
+     * @param string $stroken
+     * @return string
+     */
+    public static function getTokenConfirmation($token)
+    {
+        return sha1(self::getPasswordSalt() . $token . 'confirmation');
+    }
+
+}
