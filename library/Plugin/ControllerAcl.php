aa29c0443d5943b64cdd26cfbd88490e388ddd4b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2010-11-19 18:32:34 -0200

    Temporary commit. Workin on Plugin_ControllerAcl

--- library/Plugin/ControllerAcl.php
+++ library/Plugin/ControllerAcl.php
@@ -6,13 +6,39 @@
  */
 class Plugin_ControllerAcl extends Zend_Controller_Plugin_Abstract
 {
+    /**
+     *
+     * @var Zend_Acl
+     */
+    protected $_acl = null;
+    public function __construct()
+    {
+        $acl = new Zend_Acl();
+        $acl->addRole(new Zend_Acl_Role('guest'));
+        $acl->addRole(new Zend_Acl_Role('member'),'guest');
+        $acl->addRole(new Zend_Acl_Role('admin'),'member');
+
+        $acl->addResource(new Zend_Acl_Resource('module-default'));
+        $acl->addResource(new Zend_Acl_Resource('module-admin'),'module-default');
+        $acl->addResource(new Zend_Acl_Resource('model-admin-controller-user'),'module-admin');
+        $acl->addResource(new Zend_Acl_Resource('model-admin-controller-user-action-add'),'model-admin-controller-user');
+
+        $acl->allow('admin', 'model-admin-controller-user');
+        
+        $this->_acl = $acl;
+
+
+
+        
+    }
+
     public function routeShutdown(Zend_Controller_Request_Abstract $request)
     {
-        if ($request->getControllerName() == 'user') {
-            die('oops');
-        }
-
-
+        $user = Admin_Model_Authentication::getIdentity();
+        $role = $user->Role->name;
+        $module = $request->getModuleName();
+        $controller = $request->getControllerName();
+        $action = $request->getActionName();
         
     }
 }
